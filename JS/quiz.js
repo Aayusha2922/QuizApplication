ad3cece602301e8bd587eac001754e4f3fbc8f81
Aayusha2922 <116938194+Aayusha2922@users.noreply.github.com>
2023-01-30 22:20:59 +0530

    Add files via upload

---
+++ JS/quiz.js
@@ -0,0 +1,102 @@
+let quiz = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        option: [
+            "<javascript>",
+            "<script>",
+            "<scripting>",
+            "<js>",
+        ],
+        answer: 2,
+    },
+    {
+        question: "What does CSS stand for?",
+        option: [
+            "Colorful Style Sheets",
+            "Computer Style Sheets",
+            "Creative Style Sheets",
+            "Cascading Style Sheets",
+        ],
+        answer: 4,
+    },
+    {
+        question: "Which HTML attribute is used to define inline styles?",
+        option: [
+            "font",
+            "class",
+            "style",
+            "styles",
+        ],
+        answer: 3,
+    },
+    {
+        question: "How do you open a new window with JavaScript?",
+        option: [
+            "window.open(...);",
+            "window.new(...);",
+            "open(new window());",
+            "window.open_new(...);",
+        ],
+        answer: 1,   
+    },
+    {
+        question: "Choose the correct HTML element for the largest heading:",
+        option: [
+            "<h6>",
+            "<h1>",
+            "<head>",
+            "<heading>",
+        ],
+        answer: 2,   
+    },
+    {
+        question: "How do you insert a comment in a CSS file?",
+        option: [
+            "/* this is a comment */",
+            "// this is a comment",
+            "' this is a comment",
+            "// this is a comment //",
+        ],
+        answer: 1,   
+    },
+    {
+        question: "Which CSS property controls the text size?",
+        option: [
+            "font-style",
+            "text-style",
+            "text-size",
+            "font-size",
+        ],
+        answer: 4,   
+    },
+    {
+        question: "Which of these elements are all <table> elements?",
+        option: [
+            "<table> <tr> <td>",
+            "<table> <tr> <tt>",
+            "<table> <body> <tr>",
+            "<table> <head> <tfoot>",
+        ],
+        answer: 1,   
+    },
+    {
+        question: "How can you make a numbered list?",
+        option: [
+            "<list>",
+            "<ul>",
+            "<ol>",
+            "<dl>",
+        ],
+        answer: 3,   
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        option: [
+            "onmouseclick",
+            "onmouseover",
+            "onchange",
+            "onclick",
+        ],
+        answer: 4,   
+    },
+]
